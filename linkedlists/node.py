e31e4251668d188b2c8d70619dc95c4037d77478
GAMAL ALI <gamalali@GAMALs-MacBook-Pro.local>
2018-09-25 10:48:59 -0400

    begin ds journey: linked lists

---
+++ linkedlists/node.py
@@ -0,0 +1,32 @@
+# Create a node class
+class Node:
+    '''
+        Node Class
+            get_value
+            set_value
+            set_next_node_value
+    '''
+    def __init__(self, value, next_node=None):
+        '''
+        Constructor class
+        '''
+        self.value = value
+        self.next_node = next_node
+    
+    def get_value(self):
+        '''
+        return val of node
+        '''
+        return self.value
+    
+    def get_next_node(self):
+        '''
+        return val of next_node
+        '''
+        if self.next_node is not None:
+            return self.next_node
+
+        return None
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node

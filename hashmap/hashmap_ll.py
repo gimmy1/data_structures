2ce7b74ba9d2b2c2319dde62ffba3250a183a5c6
GAMAL ALI <gamalali@GAMALs-MacBook-Pro.local>
2018-10-08 20:06:55 -0400

    hashmap implementation with array and linked list as underlying DS

---
+++ hashmap/hashmap_ll.py
@@ -0,0 +1,54 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
+class HashMap:
+  def __init__(self, size):
+    self.array_size = size
+    self.array = [LinkedList() for i in range(self.array_size)]
+    
+  def hash(self, key):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code
+   
+  def compressor(self, hash_code):
+    return hash_code % self.array_size
+  
+  def assign(self, key, value):
+    array_index = self.compressor(self.hash(key))
+    list_at_array = self.array[array_index]
+    for item in list_at_array:
+      if key == item[0]:
+        item[1] = value
+        
+    payload = Node([key, value])
+    list_at_array.insert(payload)
+
+  
+  def retrieve(self, key):
+    array_index = self.compressor(self.hash(key))
+    list_at_index = self.array[array_index]
+    
+    for item in list_at_index:
+      if key == item[0]:
+        return item[1]
+    return None
+
+# blossom = HashMap(len(flower_definitions))
+  
+# for flower in flower_definitions:
+blossom.assign(['daisy', '1'])
+
+print(blossom.retrieve('daisy'))
+ 
+
+  
+  
+  
+    
+    
+    
+    
+    
+    
+    
